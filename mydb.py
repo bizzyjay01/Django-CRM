27f5739657e9990b4aabe5efe669ecdb4fed000e
Bisola John <bizzyjay933@gmail.com>
2024-03-16 23:48:41 +0100

    crm

---
+++ mydb.py
@@ -0,0 +1,14 @@
+# import mysql.connector
+
+# dataBase = mysql.connector.connect(
+#      host='localhost',
+#      user = 'root',
+#      passwd = 'barryallen1!'
+# )
+
+# # prepare a cursor objects
+# cursorObject = dataBase.cursor()
+
+# # create a database
+# cursorObject.execute("CREATE DATABASE crm_database")
+# print("All Done!")
