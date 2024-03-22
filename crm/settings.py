405eab63ecccfe6b97b4ba7e1089f671da96361a
Bisola John <bizzyjay933@gmail.com>
2024-03-22 19:41:30 +0100

    crm

--- crm/settings.py
+++ crm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aupez!5s-kilh9re5ms%l!8gsdnu83wq__%m8hyr^^ts%lk)&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
